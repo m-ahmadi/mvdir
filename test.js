b1100a594ba02fddb36fd1390f236375d05d51c7
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-13 01:46:10 +0330

    added test script

---
+++ test.js
@@ -0,0 +1,78 @@
+const { mkdirSync, writeFileSync, rmdirSync, unlinkSync } = require('fs');
+const mvdir = require('./');
+
+const tests = [
+  async function () {
+    mkdirSync('source');
+    writeFileSync('source/file.js', text());
+    
+    const stat = await mvdir('source/file.js', 'dest/file.js');
+    
+    unlinkSync('dest/file.js');
+    rmdirSync('dest');
+    rmdirSync('source');
+    return stat;
+  },
+  async function () {
+    mkdirSync('source');
+    writeFileSync('source/file.js', text());
+    const stat = await mvdir('source/file.js', 'dest');
+    unlinkSync('dest');
+    rmdirSync('source');
+    return stat;
+  },
+  async function () {
+    mkdirSync('source');
+    mkdirSync('source/a');
+    mkdirSync('source/b');
+    writeFileSync('source/file.js', text());
+    writeFileSync('source/a/file.js', text());
+    writeFileSync('source/b/file.js', text());
+    
+    const stat = await mvdir('source', 'dest');
+    
+    unlinkSync('dest/a/file.js');
+    unlinkSync('dest/b/file.js');
+    unlinkSync('dest/file.js');
+    rmdirSync('dest/a');
+    rmdirSync('dest/b');
+    rmdirSync('dest');
+    return stat;
+  },
+  async function () {
+    mkdirSync('source');
+    writeFileSync('source/file.js', text());
+    
+    const stat = await mvdir('source', 'a/b/c/dest');
+    
+    unlinkSync('a/b/c/dest/file.js');
+    rmdirSync('a/b/c/dest');
+    rmdirSync('a/b/c');
+    rmdirSync('a/b');
+    rmdirSync('a');
+    return stat;
+  },
+  async function () {
+    writeFileSync('file.js', text());
+    
+    const stat = await mvdir('file.js', 'D:\\file.js');
+    
+    unlinkSync('D:\\file.js');
+    return stat;
+  }
+];
+
+(async function () {
+  let i = 1;
+  for (test of tests) {
+    log( `Test ${i}: `, await test() );
+    i++;
+  } 
+})();
+
+function log(m, stat) {
+  console.log(`${m}[${stat ? '32' : '31'}m${stat ? 'passed ✔' : 'failed ✘'}[0m`);
+}
+function text() {
+  return [...Array(1000).fill('hello')].join('\n');
+}
